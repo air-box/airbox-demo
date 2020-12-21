c7bf37bba5a6355252cc3b18e93b6f1ae460b967
Ketan Bhardwaj <ketanbj@gatech.edu>
2020-12-21 13:51:06 -0500

    added utils folder

---
+++ utils/openssl-intercept-check.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+import OpenSSL
+import socket
+import struct
+
+# Prefer TLS
+context = OpenSSL.SSL.Context(OpenSSL.SSL.TLSv1_METHOD)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(5)
+connection = OpenSSL.SSL.Connection(context,s)
+connection.connect(("example.com",443))
+
+# Put the socket in blocking mode
+s.setblocking(1)
+
+# Set the timeout using the setsockopt
+#tv = struct.pack('ii', int(6), int(0))
+#connection.setsockopt(socket.SOL_SOCKET, socket.SO_RCVTIMEO, tv)
+
+print "Connected to " , connection.getpeername()
+print "Sate " , connection.get_state_string()
+
+try:
+    print "Starting Handshake"
+    connection.do_handshake()
+except OpenSSL.SSL.WantReadError:
+    print "Timeout"
+    quit()
+
+print "State " , connection.get_state_string()
+
+# Send question
+print "Transmitted %d bytes" %  connection.send("koekoek\r\n")
+
+# Expect result 
+try:
+    recvstr = connection.recv(1024)
+except OpenSSL.SSL.WantReadError:
+    print "Timeout"
+    quit()
+
+# Print response 
+print recvstr 
