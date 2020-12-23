286db4cd48560522f7c1babe25d995f9d0f7c778
ketanbj <ketanbj@air-box.io>
2020-12-23 21:50:35 +0000

    added dep.sh

--- get.sh
+++ get.sh
@@ -8,7 +8,6 @@ git clone https://github.com/air-box/keyvisor.git
 
 echo "Building keyvisor ..."
 cd keyvisor
-apt-get install -y build-essential libssl-dev
 make
 if [[ -f "keyvisor.so" ]]; then
        echo "keyvisor library generated"
@@ -17,9 +16,9 @@ fi
 cd ~/air-box
 
 echo "Getting keycentral ..."
-#https://github.com/air-box/keycentral.git
-#cd keycentral
-#make
+git clone https://github.com/air-box/keycentral.git
+cd keycentral
+make
 #check
 
 cd ~/air-box
