a6bb3019fbcc944d3c1c4c0c881a8ad00063d9ba
ketanbj <ketanbj@air-box.io>
2021-01-13 22:52:14 +0000

    find files

--- utils/find_certs_keys.sh
+++ utils/find_certs_keys.sh
@@ -17,51 +17,57 @@ crtcount=0
 keycount=0
 
 echo "------------------------"
-echo "Checking all .pem files"
-echo "------------------------"
-found_raw=$(find / -name *.pem) 
-echo "Total .pen files found:" 
-#wc -l ${found_raw}
+echo "Searching for all non-system .pem files"
 
-found = $(find / -name *.pem | grep -v -E 'docker|snap|python|Root|grep|Certification_Authority|CA|ssl|crda|ubuntu'
+find / -name *.pem
+found=$(find / -name *.pem | grep -v -E 'docker|snap|python|Root|grep|Certification_Authority|CA|ssl|crda|ubuntu')
+
 echo "Checking only non-system .pem files"
-wc -l found
 
 for file in ${found}
 do
 	((pemcount=pemcount+1))
 	spin
-	echo ${file}
+	echo "Found a non system ${file} ..."
 	openssl x509 -in ${file} -noout --text 
 	#| grep -si -E 'airbox|keyless'
 done
 #endspin
 
-
 echo "------------------------"
 echo "Checking all .crt files"
 echo "------------------------"
+find / -name *.crt
 found=$(find / -name *.crt | grep -v -E 'docker|mozilla|snap|grub|doc')
+
 for file in ${found}
 do
 	((crtcount=crtcount+1))
 	spin
-	echo ${file}
+	echo "Found a non system ${file} ..."
 	openssl x509 -in ${file} --text -noout
 done
 #endspin
 
 
 echo "------------------------"
-echo "checking for all keys"
+echo "checking for all .key files"
 echo "------------------------"
+find / -name *.key
 found=$(find / -name *.key | grep -v -E 'docker|google')
 for file in ${found}
 do
 	((keycount=keycount+1))
 	spin
-	echo ${file}
+	echo "Found a non system ${file} ..."
 	openssl rsa -in ${file} --check
 done
 endspin
+
+
+echo "------------------------"
+echo "Found ${pemcount} .pem files"
+echo "Found ${crtcount} .crt files"
+echo "Found ${keycount} .key files"
+echo "------------------------"
 
